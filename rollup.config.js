2af0d4203ec89364004f49142ce52458457abf79
luoob <liutzui@163.com>
2022-08-17 10:24:30 +0800

    chore: change config file

--- rollup.config.js
+++ rollup.config.js
@@ -1,20 +1,33 @@
 import typescript from '@rollup/plugin-typescript'
 import dts from 'rollup-plugin-dts'
+import del from 'rollup-plugin-delete'
 import { terser } from 'rollup-plugin-terser'
 import { defineConfig } from 'rollup'
 
 const publicConfig = {
     format: 'umd',
-    name: 'yyds'
+    name: 'utils'
 }
 
 const config = defineConfig([
     {
         input: 'src/index.ts',
         output: [
-            { file: './utils.js', ...publicConfig },
-            { file: './utils.min.js', ...publicConfig, plugins: [terser()] }
+            { file: 'index.js', ...publicConfig },
+            { file: 'index.min.js', ...publicConfig, plugins: [terser()] }
         ],
+        plugins: [
+            typescript({
+                declaration: false
+            })
+        ]
+    },
+    {
+        input: 'src/index.ts',
+        output: {
+            file: 'index.mjs',
+            format: 'esm'
+        },
         plugins: [
             typescript({
                 declaration: false
@@ -23,13 +36,17 @@ const config = defineConfig([
     },
     // 归并 .d.ts 文件
     {
-        input: 'esm/index.d.ts',
+        input: 'types/index.d.ts',
         output: {
             file: 'index.d.ts',
             format: 'es'
         },
         plugins: [
-            dts()
+            dts(),
+            del({
+                targets: 'types',
+                hook: 'buildEnd'
+            })
         ]
     }
 ])
